eab219271b429ebb65cfc5fef7faf3f2f6b59938
dp <andrewpeterson1@keemail.me>
2025-07-16 12:41:20 +0300

    fix: create /db folder when does not exist

--- app/models/private_key.rb
+++ app/models/private_key.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 require 'securerandom'
+require 'fileutils'
 
 module Models
   class PrivateKey
     PRIVATE_KEY_PATH = File.expand_path('../db/private-key.txt', __dir__)
+    DB_DIR = File.dirname(PRIVATE_KEY_PATH)
 
     def get
       return File.read(PRIVATE_KEY_PATH).strip if File.exist?(PRIVATE_KEY_PATH)
@@ -15,6 +17,8 @@ module Models
     private
 
     def generate_and_store
+      FileUtils.mkdir_p(DB_DIR) unless Dir.exist?(DB_DIR)
+
       hex = SecureRandom.hex(32)
       File.write(PRIVATE_KEY_PATH, hex)
 
